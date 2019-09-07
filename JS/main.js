3a2987b282599a6b15a210664d15b9764b45c6fa
SonYUA <pankivluck@gmail.com>
2019-09-07 21:43:38 +0300

    slider

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,6 @@
 'use strict'
-// function burger() {
-//   let menu = document.querySelector("#menu");
-//   let burger = document.querySelector("#burger");
-//   let burgerIconLink = document.querySelector("#burger-icon-link")
-//   let burgerIcon = document.querySelector("#burger-icon")
-//   menu.classList.add("menu-active");
-//   menu.classList.remove("menu");
-//   burger.classList.add("burger-list-active");
-//   burger.classList.remove("burger-list");
-//   burgerIconLink.classList.add("burger-icon-link-active");
-//   burgerIconLink.classList.remove("burger-icon-link");
-//   burgerIcon.classList.add("burger-icon-active");
-//   burgerIcon.classList.remove("burger-icon");
-// }
+/* BURGER */
+
 function burger() {
   let menu = document.querySelector("#menu");
   let burger = document.querySelector("#burger");
@@ -23,3 +11,57 @@ function burger() {
   burgerIconLink.className === "burger-icon-link" ? burgerIconLink.className = "burger-icon-link-active" : burgerIconLink.className = "burger-icon-link";
   burgerIcon.className === "burger-icon" ? burgerIcon.className = "burger-icon-active" : burgerIcon.className = "burger-icon";
 }
+
+
+/* SLIDER */
+let btnRight = document.querySelector("#btn-right");
+let btnLeft = document.querySelector("#btn-left");
+let images = document.querySelectorAll("#slide-inner");
+let current = 0;
+
+function slideShowleft() {
+
+ if (current < images.length - 1) {
+  for (let i =0; i < images.length; i++) {
+    images[i].classList.remove("active");
+    images[i].classList.add("slide-inner");
+    
+  }
+  current++;
+  images[current].classList.remove("slide-inner");
+   images[current].classList.add("active");
+   
+ }
+ 
+
+  else  if (current === images.length - 1) {
+      current = images.length - 1;
+    }
+
+};
+
+function slideShowRight() {
+
+ 
+
+  if (current > 0){
+
+    for (let i =0; i < images.length; i++) {
+      images[i].classList.remove("active");
+      images[i].classList.add("slide-inner");
+      
+    }
+    current--;
+  images[current].classList.remove("slide-inner");
+  images[current].classList.add("active");
+   
+  } else if (current === 0) {
+    
+    current = 0;
+  }
+}
+
+btnLeft.addEventListener('click', slideShowleft);
+btnRight.addEventListener('click',slideShowRight);
+
+
